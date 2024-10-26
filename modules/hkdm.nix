ebd00938c92dbc54e3d90b08e198553a3b9ee315
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-10-27 01:26:30 +0800

    fix: now a configFile is required by hkdm

--- modules/hkdm.nix
+++ modules/hkdm.nix
@@ -12,16 +12,30 @@
       default = false;
       description = "Enable the Hotkey Daemon for mobile devices";
     };
+    configFile = lib.mkOption {
+      type = lib.types.str;
+      default = null;
+      description = ''
+        Path to the hkdm configuration file. Example: `''${ttyescape}/etc/hkdm/config.d/ttyescape.toml`.
+      '';
+    };
   };
 
   config = lib.mkIf config.services.hkdm.enable {
+    assertions = [
+      {
+        assertion = config.services.hkdm.configFile != null;
+        message = "The option `services.hkdm.configFile` must be set when `services.hkdm.enable` is true.";
+      }
+    ];
+
     systemd.services.hkdm = {
       description = "Hotkey Daemon (For) Mobile";
       after = [ "multi-user.target" ];
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         Restart = "on-failure";
-        ExecStart = "${pkgs.hkdm}/bin/hkdm";
+        ExecStart = "${pkgs.hkdm}/bin/hkdm -i -c ${config.services.hkdm.configFile}";
       };
     };
   };
