d063c0a9cfd5696952e422514e2c357757a517b5
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-10-27 18:05:37 +0800

    snapshot

--- modules/hkdm.nix
+++ modules/hkdm.nix
@@ -1,10 +1,8 @@
 {
   config,
   lib,
-  hkdm,
   ...
 }:
-
 {
   options.services.hkdm = {
     enable = lib.mkOption {
@@ -12,12 +10,19 @@
       default = false;
       description = "Enable the Hotkey Daemon for mobile devices";
     };
+
     configFile = lib.mkOption {
       type = lib.types.str;
       default = null;
       description = ''
         Path to the hkdm configuration file. Example: `''${ttyescape}/etc/hkdm/config.d/ttyescape.toml`.
       '';
+    };
+
+    package = lib.mkOption {
+      type = lib.types.package;
+      default = null;
+      description = "The package to use for the Hotkey Daemon. Must be specified if `services.hkdm.enable` is true.";
     };
   };
 
@@ -27,6 +32,10 @@
         assertion = config.services.hkdm.configFile != null;
         message = "The option `services.hkdm.configFile` must be set when `services.hkdm.enable` is true.";
       }
+      {
+        assertion = config.services.hkdm.package != null;
+        message = "The option `services.hkdm.package` must be set when `services.hkdm.enable` is true.";
+      }
     ];
 
     systemd.services.hkdm = {
@@ -35,7 +44,7 @@
       wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         Restart = "on-failure";
-        ExecStart = "${hkdm}/bin/hkdm -i -c ${config.services.hkdm.configFile}";
+        ExecStart = "${config.services.hkdm.package}/bin/hkdm -i -c ${config.services.hkdm.configFile}";
       };
     };
   };
