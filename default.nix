3cf0155fefb19616299aee95737ce6aab0fc1c7e
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-10-27 17:35:37 +0800

    fix: nixosModules

--- default.nix
+++ default.nix
@@ -1,20 +1,14 @@
 {
-  lib,
-  config,
-  pkgs ? import <nixpkgs> { },
-  ...
-}@args:
-{
   pkgs = import ./pkgs "nur";
   nixosModules = {
-    kata-containers = import ./modules/kata-containers.nix args;
-    lyrica = import ./modules/lyrica.nix args;
-    nix-cache-attic = import ./modules/nix-cache-attic.nix args;
-    nix-cache-cachix = import ./modules/nix-cache-cachix.nix args;
-    nix-cache-garnix = import ./modules/nix-cache-garnix.nix args;
-    openssl-oqs-provider = import ./modules/openssl-oqs-provider.nix args;
-    qemu-user-static-binfmt = import ./modules/qemu-user-static-binfmt.nix args;
-    wireguard-remove-lingering-links = import ./modules/wireguard-remove-lingering-links.nix args;
-    hkdm = import ./modules/hkdm.nix { inherit lib config pkgs; };
+    kata-containers = import ./modules/kata-containers.nix;
+    lyrica = import ./modules/lyrica.nix;
+    nix-cache-attic = import ./modules/nix-cache-attic.nix;
+    nix-cache-cachix = import ./modules/nix-cache-cachix.nix;
+    nix-cache-garnix = import ./modules/nix-cache-garnix.nix;
+    openssl-oqs-provider = import ./modules/openssl-oqs-provider.nix;
+    qemu-user-static-binfmt = import ./modules/qemu-user-static-binfmt.nix;
+    wireguard-remove-lingering-links = import ./modules/wireguard-remove-lingering-links.nix;
+    hkdm = import ./modules/hkdm.nix;
   };
 }
