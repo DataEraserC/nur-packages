008c4d1f583f2259725aad7e7df20d7b42c8f3d7
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2024-12-17 17:54:03 +0800

    fix: example-package-appimage can not be built

--- pkgs/uncategorized-DataEraserC/example-package-appimage/default.nix
+++ pkgs/uncategorized-DataEraserC/example-package-appimage/default.nix
@@ -5,19 +5,20 @@
 }:
 let
   pname = "dingtalk";
-  version = "2.1.13";
+  version = "2.1.22";
   name = "${pname}-${version}";
   src = fetchurl {
     url = "https://github.com/nashaofu/dingtalk/releases/download/v${version}/${name}-latest-x86_64.AppImage";
-    sha256 = "0z1nli1f3miri3i44b725avxlp6c5pyyxlqqf80mjpkg8y8zv70c";
+    sha256 = "sha256-oKaKD+ZT5DQgwaDQD8vmlNTm6+5Ar6jr+miytqhzy9Y=";
   };
 in
 appimageTools.wrapType2 {
-  inherit name src;
+  inherit pname version src;
   meta = {
     description = "DingTalk desktop messaging application";
     homepage = "https://www.dingtalk.com/";
     license = lib.licenses.mit;
     platforms = [ "x86_64-linux" ];
+    broken = true;
   };
 }
