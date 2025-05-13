f4bb1a938953d0e2b9d3cb0b013da9db85bed694
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2025-05-13 23:31:06 +0800

    snapshot

---
+++ pkgs/uncategorized-DataEraserC/himirage_unwrapped/default.nix
@@ -0,0 +1,9 @@
+{
+  libsForQt5,
+  ...
+}@input:
+libsForQt5.callPackage ./himirage_unwrapped.nix (
+  {
+  }
+  // input
+)
---
+++ pkgs/uncategorized-DataEraserC/himirage_unwrapped/himirage_unwrapped.nix
@@ -0,0 +1,171 @@
+{
+  lib,
+  stdenv,
+  dpkg,
+  gcc,
+  libGL,
+  ffmpeg,
+  gmic,
+  cups,
+  speechd,
+  xorg,
+  nss,
+  alsa-lib,
+  unixODBC,
+  postgresql,
+  gnutar,
+  openssl,
+  gtk3,
+  pango,
+  cairo,
+
+  makeWrapper,
+  autoPatchelfHook,
+  wrapQtAppsHook,
+
+  libsForQt5,
+
+  # Qt modules
+  qtmultimedia,
+  qtlocation,
+  qtimageformats,
+  qtbase,
+  qtwebengine,
+  qt3d,
+  qtwebsockets,
+  qtsensors,
+  qtwebview,
+  qtgamepad,
+  qtserialport,
+  qtxmlpatterns,
+  qtquickcontrols2,
+  qtspeech,
+  qtserialbus,
+  qtremoteobjects,
+  qtscxml,
+  # provide libQt5Designer.so
+  qttools,
+  # provide libQt5Bluetooth.so.5 libQt5Nfc.so.5
+  qtconnectivity,
+  qtwayland,
+
+  sources,
+  ...
+}:
+let
+  SupportedPlatforms = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
+  HostPlatform = stdenv.hostPlatform.system;
+  source =
+    if lib.elem HostPlatform SupportedPlatforms then
+      sources."himirage-${HostPlatform}"
+    else
+      throw "Unsupported platform: ${HostPlatform}";
+in
+stdenv.mkDerivation {
+  inherit (source) src;
+  pname = source.pname + "-unwrapped";
+  inherit (sources.himirage-JSON) version;
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    makeWrapper
+    wrapQtAppsHook
+    dpkg
+  ];
+
+  buildInputs =
+    [
+      gcc.cc.lib
+      libGL
+      ffmpeg
+      gmic
+      cups
+      speechd
+      xorg.libXtst
+      nss
+      alsa-lib
+      unixODBC
+      postgresql
+      gnutar
+      openssl
+      gtk3
+      pango
+      cairo
+    ]
+    ++ [
+      # Qt modules
+      qtmultimedia
+      qtlocation
+      qtimageformats
+      qtbase
+      libsForQt5.fcitx5-qt
+      qtwebengine
+      qt3d
+      qtwebsockets
+      qtsensors
+      qtwebview
+      qtgamepad
+      qtserialport
+      qtxmlpatterns
+      qtquickcontrols2
+      qtspeech
+      qtserialbus
+      qtremoteobjects
+      qtscxml
+      # provide libQt5Designer.so
+      qttools
+      # provide libQt5Bluetooth.so.5 libQt5Nfc.so.5
+      qtconnectivity
+      qtwayland
+    ];
+
+  # dontWrapQtApps = true;
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p $out/lib
+    dpkg -x $src $out
+    chmod -R 755 $out
+
+    # Relocate application files
+    mv $out/opt/apps/yeeheart/himirage $out/lib/himirage
+    mv $out/usr/share $out/
+    rm -rf $out/usr $out/opt
+
+    # Remove bundled Qt libraries to avoid version mismatch
+    # rm -rf $out/lib/himirage/lib/qt/libQt5*.so*
+    # rm -rf $out/lib/himirage/plugins/{audio,bearer,geoservices,platforminputcontexts}/
+    # rm -rf $out/lib/himirage/plugins/geoservices
+    # rm -rf $out/lib/himirage/plugins/*
+    # rm -rf $out/lib/himirage/lib/qt
+
+    # Create bin directory and symlinks
+    mkdir -p $out/bin
+    ln -s $out/lib/himirage/himirage $out/bin/himirage
+    ln -s $out/lib/himirage/himirage.sh $out/bin/himirage.sh
+
+    # Update desktop entry
+    substituteInPlace $out/share/applications/himirage.desktop \
+      --replace-fail "/opt/apps/yeeheart/himirage/himirage.sh" "himirage.sh"
+
+    rm -rf $out/lib/himirage/plugins/wayland-graphics-integration-client
+    ln -s ${qtwayland}/lib/qt-*/plugins/* $out/lib/himirage/plugins/
+
+     # Wrap executable to load nix Qt libraries
+    wrapProgram $out/lib/himirage/himirage
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    homepage = "https://photosir.com";
+    mainProgram = "himirage.sh";
+    platforms = SupportedPlatforms;
+    broken = true;
+    license = lib.licenses.unfree;
+  };
+}
