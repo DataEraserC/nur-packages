a59e730645dd39fb8982980721278cb37ef0f6bd
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2026-01-11 14:37:34 +0800

    snapshot

--- pkgs/uncategorized/default.nix
+++ pkgs/uncategorized/default.nix
@@ -10,7 +10,8 @@ packages
 // {
   liboqs-unstable = callPackage ./liboqs/unstable.nix { };
   nvlax-530 = callPackage ./nvlax/nvidia-530.nix { };
-  svp-mpv = callPackage ./svp/mpv.nix { };
+  #        error: function 'wrapper' called with unexpected argument 'mpv-unwrapped'
+  # svp-mpv = callPackage ./svp/mpv.nix { };
   uesave-0_3_0 = callPackage ./uesave/0_3_0.nix { };
   mtranservercore-rs = callPackage ./linguaspark-server { };
   linguaspark-server-x86-64-v3 = callPackage ./linguaspark-server { buildArch = "x86-64-v3"; };
