04d8903a4cc9f202eabd9e83ec36abb726746f8a
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2026-09-04 22:14:05 +0800

    Adobe CS6 appimages: mark broken
    Neither the appimage-run nor the wrapType2 packaging starts the bundled
    32-bit Wine CS6 reliably, so mark both packages broken.

--- pkgs/uncategorized-DataEraserC/Adobe_Photoshop_CS6_appimage/Adobe_Photoshop_CS6_appimage.nix
+++ pkgs/uncategorized-DataEraserC/Adobe_Photoshop_CS6_appimage/Adobe_Photoshop_CS6_appimage.nix
@@ -61,6 +61,7 @@ stdenv.mkDerivation rec {
     homepage = "https://t.me/Linux_Appimages/1042";
     license = lib.licenses.unfree;
     mainProgram = "adobe_photoshop_cs6";
+    broken = true;
     platforms = [ "x86_64-linux" ];
   };
 }
